2be9b2fe9482b3fe1f929469af299f4a1440c686
Clare Lindley <clare@aire.io>
2019-03-29 16:23:49 +0000

    Initial Commit

---
+++ functional/reduce.js
@@ -0,0 +1,23 @@
+let orders = {
+    'Clare Lindley': [
+        {name: 'a', price: '1', quantity: 1},
+        {name: 'a', price: '1', quantity: 1},
+        {name: 'a', price: '1', quantity: 1}
+    ],
+    'Anna Cousins': [
+        {name: 'a', price: '1', quantity: 1},
+        {name: 'a', price: '1', quantity: 1},
+        {name: 'a', price: '1', quantity: 1}
+    ]
+};
+
+// can't do import without babel
+let fs = require('fs')
+
+let output = fs.readFileSync('data.txt', 'utf-8')
+    // 1. turn it into an array (get rid of before and after whitespace)
+    .trim()
+    .split('\n')
+    .map(line => line.split('\t'));
+
+console.log(output); // boom we have output!
