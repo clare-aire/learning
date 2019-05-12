04fdd7dc2a2f7849a3e8ad5dbadc2370f53f4719
Clare Lindley <clare@aire.io>
2019-05-12 12:13:56 +0100

    more stoof

--- promises/starwars-callback.js
+++ promises/starwars-callback.js
@@ -1,28 +1,8 @@
 const request = require('request');
 
-
-// 1. Create the Promise
-function getPlanetsWithPromise(){
-
-    return new Promise(function (resolve, reject){
-        request('https://swapi.co/api/planets/1/', function (error, response, body) {
-            if (error) reject(error);
-            else resolve(body);
-        });
-    });
-
-}
-
-// 2. Consume the Promise
-getPlanetsWithPromise().then(
-    function(body) {
-
-        let planet = JSON.parse(body);
-        let planetDescription = `The planet's name is ${planet['name']} and its population is ${planet["population"]}`
-        console.log(planetDescription);
-    });
-
-
+// 1. Get the thing and handle errors with the request
+// 2. Handle errors parsing the JSON
+// 3. if successful make another call to lookup the inhabitants and display their names
 
 function getPlanetsWithCallback(callback){
 
@@ -39,15 +19,13 @@ function getPlanetsWithCallback(callback){
 function displayPlanets(body){
 
     let planet = JSON.parse(body);
-    let planetDescription = `The planet's name is ${planet['name']} and its population is ${planet["population"]}`
+    let planetDescription = `The planet's name is ${planet['name']} and its terrain is ${planet["terrain"]}`
     console.log(planetDescription);
 }
 
-getPlanetsWithCallback(displayPlanets)
+getPlanetsWithCallback(displayPlanets);
 
-// Learn Concepts
-// Translate to ES6
-// High level patterns and design
+
 
 
 
--- promises/starwars-promise.js
+++ promises/starwars-promise.js
@@ -1,11 +1,17 @@
 const request = require('request');
 
 
+// 1. Get the thing and handle errors with the request
+// 2. Handle errors parsing the JSON
+// 3. if successful make another call to lookup the inhabitants and display their names
+
+
+
 // 1. Create the Promise
 function getPlanetsWithPromise(){
 
     return new Promise(function (resolve, reject){
-        request('https://swapi.co/api/planets/1/', function (error, response, body) {
+        request('https://swapi.co/api/planets/1/abc', function (error, response, body) {
             if (error) reject(error);
             else resolve(body);
         });
@@ -13,40 +19,21 @@ function getPlanetsWithPromise(){
 
 }
 
-// 2. Consume the Promise
-getPlanetsWithPromise().then(
+// 2. Consume the Promise (here, .then() acts like the callback)
+getPlanetsWithPromise()
+    .then(
     function(body) {
 
         let planet = JSON.parse(body);
         let planetDescription = `The planet's name is ${planet['name']} and its population is ${planet["population"]}`
         console.log(planetDescription);
-    });
+
+    }).catch(function(error){
+        console.log('IT WENT WRONG', error);
+});
 
 
-function getPlanetsWithCallback(callback){
-
-    request('https://swapi.co/api/planets/1/', function (error, response, body) {
-        if (error) {
-            console.log(error);
-        }
-        else callback(body);
-    });
-
-
-}
-
-function displayPlanets(body){
-    
-    let planet = JSON.parse(body);
-    let planetDescription = `The planet's name is ${planet['name']} and its population is ${planet["population"]}`
-    console.log(planetDescription);
-}
-
-getPlanetsWithCallback(displayPlanets)
-
-// Learn Concepts
-// Translate to ES6
-// High level patterns and design
-
+// 1. CATCH in this case catches EVERYTHING!!!! Means you have to write less error handling
+// SICK!!!
 
 
